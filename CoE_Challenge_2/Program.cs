727a2b542b7d0239410c5456fbeaea25c7ef332d
Max Cervantes <max.dominguez@bd.com>
2022-11-09 16:59:45 -0600

    initial commit

---
+++ CoE_Challenge_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CoE_Challenges
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Enter your email: ");
+            var email = Console.ReadLine();
+            CalculateVacationsFor(employeeID: email);
+        }
+
+        private static void CalculateVacationsFor(string employeeID)
+        {
+            if (!EmailUtils.IsValid(employeeID))
+            {
+                Console.WriteLine("Invalid Email Address");
+                return;
+            }
+
+            Console.WriteLine($"Calculating vacations for employee: {employeeID}");
+        }
+    }
+
+    class EmailUtils
+    {
+        public static bool IsValid(string email) => email.ToLower().EndsWith("@acme.com");
+    }
+}
